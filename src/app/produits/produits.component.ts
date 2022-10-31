a97f84d0729a337d4437c8e3281033af5b4ed16d
nadahs002 <nada.hawes002@gmail.com>
2022-10-31 16:48:25 +0100

    first commit

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
+})
+export class ProduitsComponent implements OnInit {
+
+  produits! : Produit[];
+
+  constructor(private produitService: ProduitService,
+    private router :Router,) { 
+
+    
+
+      
+  }
+
+  supprimerProduit(p: Produit)
+  {
+  let conf = confirm("Etes-vous sûr ?");
+  if (conf)
+  this.produitService.supprimerProduit(p.idProduit!).subscribe(() => {
+  console.log("produit supprimé");
+  this.SuprimerProduitDuTableau(p);
+  });
+  // this.router.navigate(['produits']).then(() => {
+  // window.location.reload();
+  // });
+  }
+
+  SuprimerProduitDuTableau(prod : Produit) {
+    this.produits.forEach((cur, index) => {
+    if(prod.idProduit=== cur.idProduit) {
+    this.produits.splice(index, 1);
+    }
+    });
+    }
+  
+
+ngOnInit(): void {
+  this.produitService.listeProduit().subscribe(prods => {
+  console.log(prods);
+  this.produits = prods;
+  });
+  }
+
+
+}
